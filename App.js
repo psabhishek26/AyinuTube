ce38ef2d4151c1a18a18da0eddee5bec60ef8af5
psabhishek26 <sploitpay.aosp.test@gmail.com>
2024-03-24 11:07:46 +0530

    Implement native stack navigator
    Signed-off-by: psabhishek26 <sploitpay.aosp.test@gmail.com>

--- App.js
+++ App.js
@@ -1,20 +1,25 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import YtPlay from './src/screens/YtPlay';
+import YtHome from './src/screens/YtHome';
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName='Home' screenOptions={{ headerShown: false }}>
+        <Stack.Screen name='Home' component={YtHome}/>
+        <Stack.Screen name='Video' component={YtPlay}/>
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
 });
